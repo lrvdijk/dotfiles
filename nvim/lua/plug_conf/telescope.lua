ed507e57b4066a7304777b2b0626d0d6ac38d508
Lucas van Dijk <info@lucasvandijk.nl>
2023-01-17 22:07:35 -0500

    Disable git_files picker

--- nvim/lua/plug_conf/telescope.lua
+++ nvim/lua/plug_conf/telescope.lua
@@ -1,20 +1,6 @@
 require('telescope').setup()
 
-local M = {}
-
-M.project_files = function()
-  local opts = {} -- define here if you want to define something
-  vim.fn.system('git rev-parse --is-inside-work-tree')
-  if vim.v.shell_error == 0 then
-    require "telescope.builtin".git_files(opts)
-  else
-    require "telescope.builtin".find_files(opts)
-  end
-end
-
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', M.project_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fs', builtin.lsp_dynamic_workspace_symbols, {})
-
-return M
