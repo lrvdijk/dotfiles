167ec4c191e43936a3d713bc16f6dc874d9acf32
Lucas van Dijk <info@lucasvandijk.nl>
2023-01-17 22:08:07 -0500

    Improved bufferline config

--- nvim/lua/plug_conf/bufferline.lua
+++ nvim/lua/plug_conf/bufferline.lua
@@ -1,6 +1,21 @@
 require("bufferline").setup {
     options = {
         numbers = "buffer_id",
-        diagnostics = "coc",
+        indicator = {
+            style = "none"
+        },
+        diagnostics = "nvim_lsp",
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "left",
+                separator = true
+            }
+        },
+        separator_style = "slant"
     },
 }
+
+vim.keymap.set("n", "<C-n>", ":BufferLineCyclePrev<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<C-m>", ":BufferLineCycleNext<CR>", { silent = true, noremap = true })
