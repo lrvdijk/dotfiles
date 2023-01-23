647d6e28e6ad3af4578b70fbd10086297f69b743
Lucas van Dijk <info@lucasvandijk.nl>
2023-01-23 08:56:18 -0500

    Add keymappings to split terminals

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,32 @@
 local wezterm = require('wezterm')
 
 return {
+    -- Font and colors
     font = wezterm.font 'Fira Code',
     font_size = 11.0,
-    color_scheme = 'DanQing (base16)'
+    color_scheme = 'DanQing (base16)',
+
+    -- Key mappings
+    leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
+    keys = {
+        -- More easy split key mappings
+        {
+            key = '|',
+            mods = 'LEADER|SHIFT',
+            action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+        },
+
+        {
+            key = '-',
+            mods = 'LEADER',
+            action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+        },
+
+        -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+        {
+            key = 'a',
+            mods = 'LEADER|CTRL',
+            action = wezterm.action.SendString '\x01',
+        },
+    },
 }
