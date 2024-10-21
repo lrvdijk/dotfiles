a3c1faa9605d3f760c50ddeae054722a19170e36
Lucas van Dijk <info@lucasvandijk.nl>
2024-10-21 09:32:07 -0400

    enh: Restore cursor after stripping whitespace

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,7 +1,12 @@
 -- Remove trailing whitespace
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
-  command = [[%s/\s\+$//e]],
+  callback = function()
+    -- Restore cursor after removing whitespace
+    local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.cmd [[%s/\s\+$//e]]
+    vim.api.nvim_win_set_cursor(0, {r, c})
+  end,
 })
 
 -- Check for external file changes
